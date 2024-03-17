a2b215fb754b12bbac9421b9b91ac14bfa4e3de1
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-03-17 21:47:29 +0530

    commit

--- 15 React Query (TankStack Query)/src/App.jsx
+++ 15 React Query (TankStack Query)/src/App.jsx
@@ -1,13 +1,21 @@
+const apiKey = "randomapikey";
 
+import { useQuery } from "@tanstack/react-query";
+
+const { data } = useQuery({
+  queryKey: ["apikey"],
+  queryFn: () => {
+    // fetch api here
+    fetch(apiKey).then((res) => res.json());
+  },
+});
 
 function App() {
-
-
   return (
     <>
-
+      <h1 className="bg-red-700 ">hello world</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

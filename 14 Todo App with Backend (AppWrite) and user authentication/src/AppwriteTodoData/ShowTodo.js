3e72f41f9c5a59235d1c0ccf6b90c62b3781fa5c
Sahil <113676978+whonix2904@users.noreply.github.com>
2024-03-26 23:22:15 +0530

    commit

---
+++ 14 Todo App with Backend (AppWrite) and user authentication/src/AppwriteTodoData/ShowTodo.js
@@ -0,0 +1,17 @@
+import { Databases } from "appwrite";
+import { client } from "../AppwriteBackend/UserAuth";
+import backendData from "../assets/env";
+
+
+export function DeleteTodo() {
+
+
+    const databases = new Databases(client)
+    const promise = databases.listDocuments(backendData.databaseID, backendData.collectionID, "documentID");
+
+    promise.then(function (response) {
+        console.log(response); // Success
+    }, function (error) {
+        console.log(error); // Failure
+    });
+}
